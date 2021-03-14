40cd25ab04bfc5112132528366cac9628491d50d
Obskurcyte <hadrien.jaubert@centrale-marseille>
2021-03-14 14:47:20 +0100

    first commit

---
+++ pages/EyeTracking.js
@@ -0,0 +1,17 @@
+import PageType from "../components/PageType";
+
+function FirstPartyData () {
+    return (
+        <div>
+            <PageType
+                title="EYE TRACKING"
+                image="https://www.50a.fr/img/upload/eye-traking.jpg"
+                description="L’Eye tracking ou “Oculométrie” désigne les techniques d’études du regard ou comportement oculaire. Il sert notamment lors de la détermination des « Zones Froides ».
+
+Il mesure le temps de fixation du regard d’un individu sur une annonce publicitaire (ou d’un rayon, d’une application mobile, d’un site). L’Eye tracking est utilisé pour conduire et réadapter sa stratégie marketing ou de communication afin d’augmenter son audience et sa visibilité."
+            />
+        </div>
+    )
+}
+
+export default FirstPartyData;
